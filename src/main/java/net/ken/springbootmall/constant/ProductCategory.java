8a6e7e69e81bcceb000765bd2452d0d63a42a5fd
KenLin <linycken210127@gmail.com>
2024-05-24 16:37:04 +0800

    feat: add product model

---
+++ src/main/java/net/ken/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package net.ken.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK,
+}
---
+++ src/main/java/net/ken/springbootmall/model/Product.java
@@ -0,0 +1,95 @@
+package net.ken.springbootmall.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import net.ken.springbootmall.constant.ProductCategory;
+
+import java.sql.Timestamp;
+
+
+public class Product {
+
+    private Integer Id;
+    private String name;
+    private ProductCategory category;
+    @JsonProperty("image_url")
+    private String imageUrl;
+    private Integer price;
+    private Integer stock;
+    private String description;
+    @JsonProperty("created_date")
+    private Timestamp createdDate;
+    @JsonProperty("last_modified_date")
+    private Timestamp lastModifiedDate;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Timestamp lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
